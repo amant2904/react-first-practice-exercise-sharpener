d6abd8696062e7ff862858bb5769c9a7750311b0
amant2904 <tiwariaman2904@gmail.com>
2023-03-02 12:39:43 +0530

    reactDom.portal used to render alertbox seperately

--- src/components/MessageBox/MessageBox.js
+++ src/components/MessageBox/MessageBox.js
@@ -1,20 +1,34 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
 import "./MessageBox.css"
+
+const BackDrop = () => {
+    return (
+        <div className="backDrop"></div>
+    )
+}
+
+const AlertBox = (props) => {
+    return (
+        <div className='message-box'>
+            <div className="heading">
+                <h1>Invalid Input</h1>
+            </div>
+            <div className="message">
+                <p>{props.msg}</p>
+            </div>
+            <div className="okay_btn">
+                <button onClick={props.btn_handler}>Okay</button>
+            </div>
+        </div>
+    )
+}
 
 export default function MessageBox(props) {
     return (
-        <div className="backDrop">
-            <div className='message-box'>
-                <div className="heading">
-                    <h1>Invalid Input</h1>
-                </div>
-                <div className="message">
-                    <p>{props.message}</p>
-                </div>
-                <div className="okay_btn">
-                    <button onClick={props.btn_handler}>Okay</button>
-                </div>
-            </div>
-        </div>
+        <React.Fragment>
+            {ReactDOM.createPortal(<BackDrop />, document.getElementById("backDrop"))}
+            {ReactDOM.createPortal(<AlertBox msg={props.message} btn_handler={props.btn_handler} />, document.getElementById("errorAlertBox"))}
+        </React.Fragment>
     )
 }
