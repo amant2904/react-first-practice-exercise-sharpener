7f27f907cac864f72f28c841d4b2fb139cc201d0
amant2904 <tiwariaman2904@gmail.com>
2023-03-02 11:02:21 +0530

    backdrop applied

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
+import React from 'react';
 import { useState } from 'react';
 import './App.css';
 import UserForm from './components/userInput/UserForm';
 import UserDetails from './components/UserDetails/UserDetails';
 import MessageBox from './components/MessageBox/MessageBox';
-import Wrapper from './components/Helpers/Wrapper';
+// import Wrapper from './components/Helpers/Wrapper';
 
 function App() {
   const [details, setDetails] = useState([]);
-  const [alertBox, setAlertBox] = useState(false);
-  const [alertMessage, setAlertMessage] = useState("");
+  const [alertBox, setAlertBox] = useState();
+  // const [alertMessage, setAlertMessage] = useState("");
   const liftUpFormValues = (username, age) => {
-    if (username.trim().length === 0 && age.trim().length === 0) {
-      setAlertMessage("You Have Typed Invalid Username and Age");
-      setAlertBox(true);
-      return;
-    }
-    else if (username.trim().length === 0) {
-      setAlertMessage("You Have Typed Invalid Username");
-      setAlertBox(true);
-      return;
-    }
-    else if (age.trim().length === 0) {
-      setAlertMessage("You Have Typed Invalid Age");
-      setAlertBox(true);
+    if (username.trim().length === 0 || age.trim().length === 0) {
+      // setAlertMessage("You Have Typed Invalid Input");
+      setAlertBox({ message: "You Have Entered Invalid Input" });
       return;
     }
     else if (parseInt(age) < 0) {
-      setAlertMessage("Age can not be less than 0");
-      setAlertBox(true);
+      // setAlertMessage("Age can not be less than 0");
+      setAlertBox({ message: "Age can not be less than 0" });
       return;
     }
-    setDetails([...details, {
-      username: username,
-      age: age,
-      id: Math.random().toString()
-    }])
-
+    setDetails((prevDetails) => {
+      return [...prevDetails, {
+        username: username,
+        age: age,
+        id: Math.random().toString()
+      }]
+    })
   }
 
   const alertBox_btn = () => {
     setAlertBox(false);
   }
   return (
-    <Wrapper>
+    // <Wrapper>
+    //   <UserForm liftUpValuesInAppJs={liftUpFormValues} />
+    //   <UserDetails allDetails={details} />
+    //   {alertBox && <MessageBox message={alertMessage} btn_handler={alertBox_btn} />}
+    // </Wrapper>
+
+
+    // react fragment is an alternate of wrapper component which can be used to wrap jsx code. it can also be used directly <fregment></fregment> but for that we have to import that from react inside curly braces.
+    <React.Fragment>
       <UserForm liftUpValuesInAppJs={liftUpFormValues} />
       <UserDetails allDetails={details} />
-      {alertBox && <MessageBox message={alertMessage} btn_handler={alertBox_btn} />}
-    </Wrapper>
+      {alertBox && <MessageBox message={alertBox.message} btn_handler={alertBox_btn} />}
+    </React.Fragment>
   )
 }
 
